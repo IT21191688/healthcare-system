508a756b771f98724739793a5b43df5ba2c9c70c
IT21191688 <IT21191688@my.sliit.lk>
2024-01-18 20:31:09 +0530

    controller setup

--- src/main/java/com/cus/healthcare/system/api/DoctorController.java
+++ src/main/java/com/cus/healthcare/system/api/DoctorController.java
@@ -1,5 +1,42 @@
 package com.cus.healthcare.system.api;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController //this is a rest controller
+@RequestMapping("/api/v1/doctors")//api path
 public class DoctorController {
+
+    @PostMapping //post methord
+    public String createDoctor(){
+
+        return "create Doctor";
+    }
+
+    @GetMapping
+    public String getDoctor(){
+
+        return "Get Doctor";
+    }
+
+
+    @PutMapping
+    public String updateDoctor(){
+
+        return "Update Doctor";
+    }
+
+    @DeleteMapping
+    public String deleteDoctor(){
+
+        return "Delete Doctor";
+    }
+
+    @GetMapping(path = "/list")
+    public String getDoctorList(){
+
+        return "List Doctor";
+    }
+
 
 }
